005119cbedc8c4d830cc50a280959095d10948ac
jack2713 <jack2.fan@outlook.com>
2024-11-08 13:31:39 +0800

    Update update1.py

--- update1.py
+++ update1.py
@@ -60,8 +60,8 @@ def fetch_and_replace(urls):
 if __name__ == "__main__":  
     # 定义多个URL  
     urls = [
+        'https://notabug.org/kan314/dd/src/main/直播X.txt',
         'https://qu.ax/HtMB.txt',
-         'https://notabug.org/kan314/dd/src/d16ba60cfe15416836aad83cd8f39346942b02d3/直播.txt',
         'https://raw.githubusercontent.com/SSM0415/apptest/main/TVonline.txt',  
         'https://raw.githubusercontent.com/SSM0415/apptest/refs/heads/main/TVbox2livefomi243.txt', 
     ]  
