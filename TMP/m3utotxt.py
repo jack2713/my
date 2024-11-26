07994db0809ddf7f574d8243068d2146146788bf
jack2713 <jack2.fan@outlook.com>
2024-11-26 11:00:55 +0800

    Update m3utotxt.py

--- TMP/m3utotxt.py
+++ TMP/m3utotxt.py
@@ -58,6 +58,7 @@ if __name__ == "__main__":
         'https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u',  # 注意：修正了URL，以直接获取文件内容  
         'https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv6.m3u',  # 注意：修正了URL，以直接获取文件内容
         'https://qu.ax/kBip.m3u',
+        'https://huangsuming.github.io/iptv/list/tvlist.txt',
     ]  
   
     # 指定输出文件的名称  
