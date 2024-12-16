d17c7633f0f1bc72166b85ce2d22b65b747a971c
jack2713 <jack2.fan@outlook.com>
2024-12-16 09:23:18 +0800

    Update update.py

--- update.py
+++ update.py
@@ -62,7 +62,6 @@ if __name__ == "__main__":
     urls = [
         'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/my03.txt',
         'http://rihou.cc:555/gggg.nzk',
-        'http://rihou.cc:567/qtsd',
         'https://aktv.top/live.txt',
         'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/TMP1.txt',
         'https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt',
