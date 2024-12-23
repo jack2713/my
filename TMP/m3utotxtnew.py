82c490a8a720061c5b290ebd96b0f3c7402e3f1f
jack2713 <jack2.fan@outlook.com>
2024-12-23 16:03:18 +0800

    Update m3utotxtnew.py

--- TMP/m3utotxtnew.py
+++ TMP/m3utotxtnew.py
@@ -5,6 +5,7 @@ import re
 
 # 文件 URL 列表
 urls = [
+    'https://raw.githubusercontent.com/clion007/livetv/refs/heads/main/m3u/scu.m3u',
     'https://raw.githubusercontent.com/fanmingming/live/refs/heads/main/tv/m3u/ipv6.m3u',
     'https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u',
     'https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv6.m3u',
