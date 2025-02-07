719966610ea64a9422395f6f44103875cd23b015
jack2713 <jack2.fan@outlook.com>
2025-02-07 11:25:11 +0800

    Update m3utotxtnew.py

--- TMP/m3utotxtnew.py
+++ TMP/m3utotxtnew.py
@@ -14,7 +14,7 @@ urls = [
     'https://raw.githubusercontent.com/suxuang/myIPTV/main/ipv6.m3u',
     # 'https://qu.ax/kBip.m3u',
     'https://huangsuming.github.io/iptv/list/tvlist.txt',
-    'https://raw.githubusercontent.com/mavin521/syiptv/main/live.m3u',
+    # 'https://raw.githubusercontent.com/mavin521/syiptv/main/live.m3u',
 ]
 
 # 初始化字典
