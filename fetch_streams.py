d21108550d98bdb1b7b675330fb3cd17f6cf72af
jack2713 <jack2.fan@outlook.com>
2025-04-17 12:30:52 +0800

    Update fetch_streams.py

--- fetch_streams.py
+++ fetch_streams.py
@@ -60,11 +60,11 @@ def fetch_and_replace(urls):
 if __name__ == "__main__":
     # 定义多个URL
     urls = [
-        'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/my03.txt',
-         'http://rihou.cc:555/gggg.nzk',
+        'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/my03.txt',        
         #'http://aktv.top/live.txt',
         #'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/dy.txt',
         'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/TMP1.txt',
+        'http://rihou.cc:555/gggg.nzk',
         'https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt',
        # 'http://47.99.102.252/live.txt',
         #'http://kxrj.site:55/lib/kx2024.txt',
