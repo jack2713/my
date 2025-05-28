ee9ee69b2bbacd6f550483fc6de37a8b9a9b679c
jack2713 <jack2.fan@outlook.com>
2025-05-28 10:45:50 +0800

    Update fetch_streams.py

--- fetch_streams.py
+++ fetch_streams.py
@@ -66,6 +66,8 @@ if __name__ == "__main__":
         'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/TMP1.txt',
         'http://rihou.cc:555/gggg.nzk',
         'https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/bbxx.txt',
+        'https://live.hacks.tools/tv/iptv6.txt',
+        'https://live.hacks.tools/tv/iptv4.txt',
        # 'http://47.99.102.252/live.txt',
         #'http://kxrj.site:55/lib/kx2024.txt',
         # 'https://raw.githubusercontent.com/Supprise0901/TVBox_live/refs/heads/main/live.txt',
