3f4f03c4362e0172b5b0b2b0aacb695bb9fcdd90
jack2713 <jack2.fan@outlook.com>
2025-09-27 05:37:31 +0800

    Update m3utotxtnew.py

--- TMP/m3utotxtnew.py
+++ TMP/m3utotxtnew.py
@@ -10,7 +10,7 @@ urls = [
     #'https://raw.githubusercontent.com/big-mouth-cn/tv/refs/heads/main/iptv-ok.m3u',
     'https://live.hacks.tools/iptv/categories/movies.m3u',
     #'https://raw.githubusercontent.com/kilvn/iptv/refs/heads/master/iptv%2B.m3u',
-    'https://iptv.catvod.com/tv.m3u',
+    'https://live.catvod.com/tv.m3u',
     'https://sub.ottiptv.cc/iptv.m3u',
     'https://sub.ottiptv.cc/douyuyqk.m3u',
     'https://sub.ottiptv.cc/huyayqk.m3u',
