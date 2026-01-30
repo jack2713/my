07004a92802d2d5613867d32a634117c7ab3f00f
jack2713 <jack2.fan@outlook.com>
2026-01-30 14:50:39 +0800

    Update fetch_streams.py

--- fetch_streams.py
+++ fetch_streams.py
@@ -206,6 +206,7 @@ def main():
         'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/mytemp01.txt',
         #'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/TMP.txt',
         'https://raw.githubusercontent.com/jack2713/mynew/refs/heads/main/my2.txt',
+        'https://raw.githubusercontent.com/jack2713/mynew/refs/heads/main/my1.txt',
         'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/1699.txt',
         'http://bxtv.3a.ink/live.txt',  # 不会转换
         'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/TMP1.txt',
