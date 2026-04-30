6704513ebf774a07c061eb326d35c1bc003835e8
jack2713 <jack2.fan@outlook.com>
2026-04-30 16:47:34 +0800

    Update fetch_streams.py

--- fetch_streams.py
+++ fetch_streams.py
@@ -172,7 +172,7 @@ def main():
         'https://raw.githubusercontent.com/jack2713/mynew/refs/heads/main/my2.txt',
         'https://raw.githubusercontent.com/jack2713/mynew/refs/heads/main/my3.txt',
         'https://raw.githubusercontent.com/jack2713/mynew/refs/heads/main/my1.txt',
-        'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/1699.txt',
+        #'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/1699.txt',
         'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/TMP.txt',
         'https://raw.githubusercontent.com/jack2713/mynew/refs/heads/main/TMP/s.txt',
         'http://iptv.4666888.xyz/FYTV.txt',
