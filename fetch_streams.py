eff6692b9b7e31185772e4df45e3de5a2b100e94
jack2713 <jack2.fan@outlook.com>
2026-06-23 15:05:19 +0800

    Update fetch_streams.py

--- fetch_streams.py
+++ fetch_streams.py
@@ -184,6 +184,7 @@ def main():
         'https://raw.githubusercontent.com/jack2713/mynew/refs/heads/main/zubo.txt',
         #'https://raw.githubusercontent.com/develop202/migu_video/refs/heads/main/interfaceTXT.txt',
         'https://raw.githubusercontent.com/jack2713/mynew/refs/heads/main/rihou.txt',
+        'https://raw.githubusercontent.com/jack2713/mynew/refs/heads/main/jqcy.txt',
         'https://raw.githubusercontent.com/jack2713/mynew/refs/heads/main/TMP/jsontxt.txt',
         'https://raw.githubusercontent.com/jack2713/my/refs/heads/main/TMP/ys.txt',
         'https://raw.githubusercontent.com/swhtv/1/refs/heads/main/swtvlive',
